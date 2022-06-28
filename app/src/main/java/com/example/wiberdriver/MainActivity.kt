0b05cf7f1b721fdf26290fc593ea4058043c56d5
LamThinhPhat <92035772+LamThinhPhat@users.noreply.github.com>
2022-06-28 22:25:20 +0700

    Add sign in page for driver

--- app/src/main/java/com/example/wiberdriver/MainActivity.kt
+++ app/src/main/java/com/example/wiberdriver/MainActivity.kt
@@ -2,10 +2,17 @@ package com.example.wiberdriver
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.wiberdriver.fragments.SignInFragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragment_main_container, SignInFragment())
+            .commit();
+
+        supportActionBar?.hide()
     }
 }
