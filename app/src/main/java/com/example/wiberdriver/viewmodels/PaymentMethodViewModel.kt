c5b4d30e3a78fc79d0605e5c4c96f64dc0228463
Hoang Huu Giap <hoanghuugiap241@gmail.com>
2022-07-10 15:11:06 +0700

    add manage payment method

---
+++ app/src/main/java/com/example/wiberdriver/viewmodels/PaymentMethodViewModel.kt
@@ -0,0 +1,20 @@
+package com.example.wiberdriver.viewmodels
+
+import androidx.lifecycle.ViewModel
+import com.example.wiberdriver.models.Payment
+import java.util.*
+import kotlin.collections.ArrayList
+
+class PaymentMethodViewModel: ViewModel() {
+    fun getPaymentLists(): ArrayList<Payment> {
+        return arrayListOf<Payment>(
+            Payment("Nguyen Van A", "visa", "4000 1234 5678 9012", Date()),
+            Payment("Nguyen Van B", "visa", "4000 1234 5678 9012", Date()),
+            Payment("Nguyen Van A", "visa", "4000 1234 5678 9012", Date()),
+            Payment("Nguyen Van A", "visa", "4000 1234 5678 9012", Date()),
+            Payment("Nguyen Van B", "visa", "4000 1234 5678 9012", Date()),
+            Payment("Nguyen Van A", "visa", "4000 1234 5678 9012", Date()),
+            Payment("Nguyen Van B", "visa", "4000 1234 5678 9012", Date()),
+        )
+    }
+}
