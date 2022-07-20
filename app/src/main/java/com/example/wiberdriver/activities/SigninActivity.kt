408165acffb9d9a5e0df8915daf35e0d43001a54
LamThinhPhat <92035772+LamThinhPhat@users.noreply.github.com>
2022-07-20 21:39:27 +0700

    Fix to background

--- app/src/main/java/com/example/wiberdriver/activities/SigninActivity.kt
+++ app/src/main/java/com/example/wiberdriver/activities/SigninActivity.kt
@@ -1,12 +1,19 @@
 package com.example.wiberdriver.activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.wiberdriver.R
+import com.example.wiberdriver.databinding.ActivitySigninBinding
 
 class SigninActivity : AppCompatActivity() {
+    private lateinit var binding: ActivitySigninBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_signin)
+        binding = ActivitySigninBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.signinbtn.setOnClickListener {
+            startActivity(Intent(this@SigninActivity, HomeActivity::class.java))
+        }
     }
 }
