148bc6f24fad02e932f2c17870dd46019263b9fc
lamthinhphat <92035772+LamThinhPhat@users.noreply.github.com>
2022-07-23 12:10:43 +0700

    Phat/api/update driver info (#11)
    * Fix to background
    
    * Call API to sign in and sign up
    
    * Call API update driver info
    
    * Check logic in updating profile

---
+++ app/src/main/java/com/example/wiberdriver/models/DriverInfo.kt
@@ -0,0 +1,10 @@
+package com.example.wiberdriver.models
+
+class DriverInfo  (
+    var id: String,
+    var phone: String,
+    var name : String,
+    var role : roleEnum = roleEnum.CUSTOMER
+){
+
+}
